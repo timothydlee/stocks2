6fadc70f93765ced70602ad111ac0ef1bc158c78
Tim Lee <timothy.d.lee@live.com>
2018-03-08 00:00:23 -0500

    Initial set up - created table view

---
+++ stocks2/stocks2/StocksTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stocks2
+//
+//  Created by Timothy Lee on 3/7/18.
+//  Copyright © 2018 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class StocksTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
